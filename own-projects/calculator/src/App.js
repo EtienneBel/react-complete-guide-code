341e65c0b860c4db804ce3a4ef6862a55c2f5ecc
Etienne Wend-Kuuni Belemgnegre <ebelemgnegre@nine7soft.com>
2023-04-08 12:44:46 +0000

    feat: add calculator app

--- own-projects/calculator/src/App.js
+++ own-projects/calculator/src/App.js
@@ -1,28 +1,7 @@
-import ToDoList from "./components/ToDoList/ToDoList";
-
 function App() {
-  const data = [
-    {
-      title: "Learn Javascript",
-      description: "Lorum ipsum ...",
-      status: "Completed",
-    },
-    {
-      title: "Learn React",
-      description: "Lorum ipsum ...",
-      status: "In Progress",
-    },
-    {
-      title: "Build a React App",
-      description: "Lorum ipsum ...",
-      status: "To Do",
-    },
-  ];
-
   return (
     <div>
-      <h1>THINGS TO DO</h1>
-      <ToDoList data={data} />
+      <h1>CALCULATOR APP</h1>
     </div>
   );
 }
