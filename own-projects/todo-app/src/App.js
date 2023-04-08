64a7099bfc4e84e76a7c1159e0857db69c058112
Etienne Wend-Kuuni Belemgnegre <ebelemgnegre@nine7soft.com>
2023-04-08 10:16:45 +0000

    feat: add todo app

---
+++ own-projects/todo-app/src/App.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+function App() {
+  return <h1>To Do App</h1>;
+}
+
+export default App;
